2e302093b05c3fae2f0f998bc2ac898aff935555
QuentinSpace <pro.quentint@gmail.com>
2024-06-01 03:11:03 +0200

    u

--- index.ts
+++ index.ts
@@ -1,46 +1,17 @@
-import TwitchAPI from './module/TwitchAPI';
+import TwitchAPIEmit from './module/TwitchAPI';
 
-const client = new TwitchAPI();
+export const client = new TwitchAPIEmit();
 
 (async () => {
 	client.login();
 
-	client.onMessage(
-		(channel: string, tags: any, message: string, connection: any) => {
-			console.log('fff');
-		}
-	);
+	client.handleMessage((connection: any) => {
+		console.log('message');
+	});
 })();
 
-// const WebSocketClient = require('websocket').client;
+// client.login();
 
-// const client = new WebSocketClient();
-// const parser = require('./module/IRCParser');
-// const { parseMessages } = require('./module/parser');
-
-// client.on('connect', (ws: any) => {
-// 	console.log('WebSocket Client Connected');
-
-// 	ws.sendUTF('CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands');
-// 	ws.sendUTF('PASS oauth:04phnd1f3le44q88i37knxv3784rb7');
-// 	ws.sendUTF('NICK personastudio');
-// 	ws.sendUTF('JOIN #personastudio');
-
-// 	ws.on('message', (message: any) => {
-// 		parseMessage(message);
-// 	});
+// client.handleMessage((connection: any) => {
+// 	console.log('message');
 // });
-
-// client.connect('ws://irc-ws.chat.twitch.tv:80');
-
-// function parseMessage(messages: any) {
-// 	if (messages.type !== 'utf8') return;
-// 	let rawIrcMessage = messages.utf8Data.trimEnd();
-// 	const parse = parseMessages(rawIrcMessage);
-// 	if (parse === null) return;
-// 	const message = parse.parameters;
-// 	const channel = parse.command.channel;
-// 	const tags = parse.tags;
-
-// 	console.log(`[${channel}] ${tags['display-name']}: ${message}`);
-// }
--- module/TwitchAPI.ts
+++ module/TwitchAPI.ts
@@ -2,55 +2,43 @@ const WebSocketClient = require('websocket').client;
 
 export default class TwitchAPI {
 	private twitchClient = new WebSocketClient();
-	public connection: any;
+	private _connection: any;
 
-	constructor() {
-		this.twitchClient.on('connect', (connection: any) => {
-			console.log('WebSocket Client Connected');
-			connection.sendUTF(
-				'CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands'
-			);
-			connection.sendUTF('PASS oauth:04phnd1f3le44q88i37knxv3784rb7');
-			connection.sendUTF('NICK personastudio');
-			connection.sendUTF('JOIN #PersonaStudio');
-			this.connection = connection;
-		});
-	}
+	constructor() {}
 
 	login() {
-		this.twitchClient.connect('wss://irc-ws.chat.twitch.tv:443');
+		return new Promise((resolve, reject) => {
+			this.twitchClient.on('connect', (connection: any) => {
+				console.log('WebSocket Client Connected');
+				connection.sendUTF(
+					'CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands'
+				);
+				connection.sendUTF('PASS oauth:04phnd1f3le44q88i37knxv3784rb7');
+				connection.sendUTF('NICK personastudio');
+				connection.sendUTF('JOIN #PersonaStudio');
+				this._connection = connection;
+			});
+
+			this.twitchClient.connect('wss://irc-ws.chat.twitch.tv:443');
+
+			this._openConnection();
+		});
 	}
 
-	onMessage(callback: any) {
-		this.connection.on('message', (ircMessage: any) => {
-			callback('test', 'test', 'test', 'test');
+	_openConnection() {
+		this._connection = this._onMessage.bind(this);
+		console.log('open connection');
+	}
+
+	_onMessage(event: any) {
+		console.log('parts');
+		// this.handleMessage();
+	}
+
+	handleMessage(connection: any) {
+		this._connection.on('message', function (ircMessage: any) {
+			console.log('message');
 		});
-		// this.twitchClient.on('connect', (connection: any) => {
-		// 	console.log('WebSocket Client Connected');
-		// 	connection.sendUTF(
-		// 		'CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands'
-		// 	);
-		// 	connection.sendUTF('PASS oauth:cnqrgxxm4uffqrybm1niqmqpaog20j');
-		// 	connection.sendUTF('NICK personastudio');
-		// 	connection.sendUTF('JOIN #LeMaraudeurTV,#MooyaDEV');
-		// 	connection.on('message', function (ircMessage: any) {
-		// 		if (ircMessage.type !== 'utf8') return;
-		// 		let rawIrcMessage = ircMessage.utf8Data.trimEnd();
-		// 		console.log(
-		// 			`Message received (${new Date().toISOString()}): '${rawIrcMessage}'\n`
-		// 		);
-		// 		const parse = parseMessages(rawIrcMessage);
-		// 		if (parse === null) return;
-		// 		if (parse.command === null) return;
-		// 		if (parse.command.command !== 'PRIVMSG') return;
-		// 		console.log('Received: ' + ircMessage.utf8Data);
-		// 		callback(
-		// 			parse.command.channel,
-		// 			parse.tags,
-		// 			parse.parameters,
-		// 			connection
-		// 		);
-		// 	});
-		// });
+		console.log('mdr');
 	}
 }
