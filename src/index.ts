1b6a3f7676ac46352103be7db9ff069bed3d3db7
QuentinSpace <pro.quentint@gmail.com>
2024-07-18 02:55:48 +0200

    e

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,80 @@
+import path from 'path';
 import TwitchAPI from './module/TwitchAPI';
+const cookiesPath = path.resolve(__dirname, 'cookies.json');
 
-export const client = new TwitchAPI();
+export const twitchApi = new TwitchAPI();
 
-// (async () => {
-client.client.on(
-	'message',
-	(channel: any, tags: any, message: any, self: any) => {
-		if (self) return;
-		if (message === '!hello') {
-			client.client.say(channel, `@${tags.username}, heya!`);
+(async () => {
+	twitchApi.client.on(
+		'message',
+		async (channel: any, tags: any, message: any, self: any) => {
+			if (self) return;
+
+			if (message === '!hello') {
+				const announcements = await twitchApi.SendChatAnnouncement({
+					broadcaster_id: '1103788558',
+					moderator_id: '1045274424',
+				});
+			}
+
+			if (message === '!a') {
+				const announcements = await twitchApi.GetUsers({
+					name: 'quentinlabs',
+				});
+				console.log(announcements);
+			}
+
+			if (message === '!c') {
+				const channelInformation = await twitchApi.GetChannelInformation({
+					broadcaster_id: '1103788558',
+				});
+				console.log(channelInformation);
+			}
+
+			// if (message === '!game') {
+			// 	const channelInformation = await twitchApi.GetChannelInformation({
+			// 		broadcaster_id: '1045274424',
+			// 	});
+			// 	console.log(channelInformation);
+			// 	// client.client.say(channel, `@${tags.username}, heya!`);
+			// }
+			// if (message === '!clip') {
+			// 	const clip = await twitchApi.CreateClip({
+			// 		broadcaster_id: '1045274424',
+			// 	});
+
+			// 	console.log(clip);
+
+			// 	const browser = await puppeteer.launch({
+			// 		headless: false,
+			// 		executablePath:
+			// 			'/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/Contents/MacOS/Safari',
+			// 	});
+			// 	const page = await browser.newPage();
+
+			// 	await page.goto(clip.edit_url);
+
+			// 	await page.type('#cmgr-title-input', 'Test Clip mdr');
+
+			// 	await page.click(
+			// 		'.ScCoreButton-sc-ocjdkq-0 ScCoreButtonPrimary-sc-ocjdkq-1 itFOsv gmCwLG'
+			// 	);
+
+			// 	// await page.waitForNavigation();
+
+			// 	// await browser.close();
+			// }
 		}
-	}
-);
-// })();
+	);
+})();
+
+const Test = () => {
+	setTimeout(async () => {
+		const announcements = await twitchApi.SendChatAnnouncement({
+			broadcaster_id: '1103788558',
+			moderator_id: '1045274424',
+		});
+		Test();
+	}, 10000);
+};
+Test();
--- src/module/TwitchAPI.ts
+++ src/module/TwitchAPI.ts
@@ -1,16 +1,102 @@
 const tmi = require('tmi.js');
+import axios from 'axios';
 
 export default class TwitchAPI {
+	private apiURL = 'https://api.twitch.tv/helix';
+	private BarearToken = 'kaegoabz7slabov1aq3uyu5dt3t4hg';
+	// private BarearToken = 'et70fzndd8cvbymdayf7kbslf7c4ue';
+	private ClientID = 'gp762nuuoqcoxypju8c569th9wz7q5';
+
 	public client = new tmi.Client({
 		options: { debug: true },
 		identity: {
 			username: 'personastudio',
 			password: 'oauth:04phnd1f3le44q88i37knxv3784rb7',
 		},
-		channels: ['personastudio', 'lemaraudeurtv'],
+		channels: ['quentinlabs', 'lemaraudeurtv'],
 	});
 
 	constructor() {
 		this.client.connect();
+	}
+
+	/**
+	 * Get Channel Information
+	 * @description Get information about one channel.
+	 */
+	async GetChannelInformation({ broadcaster_id }: { broadcaster_id: string }) {
+		const result = await axios.get(
+			`${this.apiURL}/channels?broadcaster_id=${broadcaster_id}`,
+			{
+				headers: {
+					Authorization: `Bearer ${this.BarearToken}`,
+					'Client-ID': this.ClientID,
+				},
+			}
+		);
+		return result.data.data[0];
+	}
+
+	/**
+	 * Get Users
+	 * @description Get information about one user.
+	 */
+	async GetUsers({ name }: { name: string }) {
+		const result = await axios.get(`${this.apiURL}/users`, {
+			headers: {
+				Authorization: `Bearer ba6ll8i29b0xa14nxgkf6jy9o658z4`,
+				'Client-ID': 'gp762nuuoqcoxypju8c569th9wz7q5',
+			},
+		});
+		console.log(result);
+		return result.data.data[0];
+	}
+
+	/**
+	 * Send Chat Announcement
+	 * @description Get information about one user.
+	 */
+	async SendChatAnnouncement({
+		broadcaster_id,
+		moderator_id,
+	}: {
+		broadcaster_id: string;
+		moderator_id: string;
+	}) {
+		const result = await axios.post(
+			`${this.apiURL}/chat/announcements?broadcaster_id=${broadcaster_id}&moderator_id=${moderator_id}`,
+			{
+				message:
+					'Rejoins-nous sur le Discord, il est tellement cool : https://discord.gg/test',
+				color: 'primary',
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${this.BarearToken}`,
+					'Client-ID': this.ClientID,
+				},
+			}
+		);
+		// return result.data.data[0];
+	}
+
+	/**
+	 * Create Clip
+	 * @description Create a clip from the broadcaster’s stream.
+	 */
+	async CreateClip({ broadcaster_id }: { broadcaster_id: string }) {
+		const result = await axios.post(
+			`${this.apiURL}/clips?broadcaster_id=${broadcaster_id}`,
+			{
+				broadcaster_id,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${this.BarearToken}`,
+					'Client-ID': this.ClientID,
+				},
+			}
+		);
+		return result.data.data[0];
 	}
 }
